e9fed318a147822cace7a95e6b32ac3a46c29858
Dari Alves <darieltondf@gmail.com>
2022-07-24 01:02:31 -0300

    :sunglasses: header, menu hamburger, javascript e refatoracao do codigo

--- main.js
+++ main.js
@@ -1,12 +1,38 @@
 import './style.css';
 
 const links = document.querySelectorAll('.nav__link');
+const linksMobile = document.querySelectorAll('.nav__link--mobile');
+const hamburger = document.querySelector('.menu__hamburger');
+const navMobile = document.querySelector('.nav__mobile');
+const lines = document.querySelectorAll('.line');
 
-links.forEach(link => {
-  link.addEventListener('click', () => {
-    links.forEach(link => {
-      link.classList.remove('nav__link--active');
+function activeLinks(element, className) {
+  element.forEach(link => {
+    link.addEventListener('click', () => {
+      element.forEach(link => {
+        link.classList.remove(`${className}--active`);
+      });
+      link.classList.add(`${className}--active`);
     });
-    link.classList.add('nav__link--active');
   });
+}
+
+activeLinks(links, 'nav__link');
+activeLinks(linksMobile, 'nav__link--mobile');
+
+function addEvent(method) {
+  navMobile.classList[`${method}`]('menu__mobile--active');
+  lines.forEach((line, index) => {
+    line.classList[`${method}`](`line-${index + 1}--active`);
+  });
+}
+
+hamburger.addEventListener('click', () => {
+  addEvent('toggle');
+});
+
+window.addEventListener('resize', e => {
+  if (e.target.innerWidth > 768) {
+    addEvent('remove');
+  }
 });
